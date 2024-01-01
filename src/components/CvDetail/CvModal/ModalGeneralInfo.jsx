6b62cd1df6bae468bbd2329c0ed58d1899f6e7a1
duongnt <duongnt247@gmail.com>
2024-01-02 00:25:39 +0700

    fix load dob in craete cv

--- src/components/CvDetail/CvModal/ModalGeneralInfo.jsx
+++ src/components/CvDetail/CvModal/ModalGeneralInfo.jsx
@@ -11,6 +11,7 @@ import { apiCandidateInfo } from "src/apis/apiEndpoint";
 import DatePickerForm from "src/commons/FormInput/DatePickerForm";
 import SelectForm from "src/commons/FormInput/SelectForm";
 import CvModalLayout from "./CvModalLayout";
+import moment from "moment";
 
 const ModalGeneralinfo = ({ data, open, handleClose }) => {
 	const { register, handleSubmit, control, reset } = useForm();
@@ -44,6 +45,7 @@ const ModalGeneralinfo = ({ data, open, handleClose }) => {
 				email: data?.email,
 				genderId: data?.genderId,
 				location: data?.location,
+				dob: moment(data?.dob),
 			});
 	}, [data, reset]);
 
