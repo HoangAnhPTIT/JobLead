cffa669ecd230372306e382f2fe7cd7ec2b906b2
DuongNT <duongnt38@msb.com.vn>
2024-05-07 13:38:24 +0700

    buy potential customer

--- src/apis/apiEndpoint.js
+++ src/apis/apiEndpoint.js
@@ -63,3 +63,4 @@ export const apiCategory = "/Category";
 export const apiFilterCategory = `${apiCategory}/filter`;
 export const apiProvince = "/Address/Provinces";
 export const apiCustomer = "/Customer";
+export const apiBuyCustomerInfo = "/Company/view/customer";
--- src/components/PotentialClients/Category.jsx
+++ src/components/PotentialClients/Category.jsx
@@ -1,12 +1,17 @@
 "use client";
-import { UserOutlined } from "@ant-design/icons";
+import { ExclamationCircleOutlined, UserOutlined } from "@ant-design/icons";
 import { Pagination } from "@mui/material";
-import { Avatar, Button, Col, Image, Modal, Row } from "antd";
+import { Avatar, Button, Col, Image, Modal, Row, Spin } from "antd";
 import { isEmpty } from "lodash";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { httpAuthGet } from "src/apis/apiAuthCaller";
-import { apiFilterCategory } from "src/apis/apiEndpoint";
+import { toast } from "react-toastify";
+import { httpAuthGet, httpAuthPost } from "src/apis/apiAuthCaller";
+import {
+	apiBuyCustomerInfo,
+	apiCustomer,
+	apiFilterCategory,
+} from "src/apis/apiEndpoint";
 import Nodata from "src/commons/Nodata";
 import { imageError } from "src/constants/common";
 import routeMap from "src/constants/routeMap";
@@ -16,21 +21,26 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 const categoryIdDefault = "00000000-0000-0000-0000-000000000000";
 
-const ShowAvatar = ({ src, ...option }) => (
-	<Avatar
-		icon={src ? <Image src={src} alt="" preview={false} /> : <UserOutlined />}
-		shape="circle"
-		size={64}
-		{...option}
-	/>
-);
+const ItemInfo = ({ label, value }) => {
+	return (
+		<Row gutter={16} className="my-1 border-b">
+			<Col span={8}>{label}</Col>
+			<Col span={16}>
+				<span className="text-base">{value}</span>
+			</Col>
+		</Row>
+	);
+};
 
 const Category = ({ title, list, count }) => {
+	const [modal, contextHolder] = Modal.useModal();
 	const searchParams = useSearchParams();
 	const pathname = usePathname();
 	const router = useRouter();
 	const [itemSelected, setItemSelected] = useState();
 	const [categories, setCategories] = useState();
+	const [loading, setLoading] = useState(false);
+	console.log("itemSelected", itemSelected);
 
 	const onChangePage = async (page) => {
 		const searchParamsObject =
@@ -50,12 +60,44 @@ const Category = ({ title, list, count }) => {
 		router.push(`${routeMap.potentialClients}?categoryId=${id}`);
 	};
 
+	const onBuyInfo = async () => {
+		setLoading(true);
+		try {
+			const response = await httpAuthPost({
+				endpoint: apiBuyCustomerInfo,
+				data: { CustomerId: itemSelected?.id },
+			});
+			if (response?.status === 200) {
+				toast.success("Mua thông tin thành công");
+			} else {
+				toast.error(response?.message);
+			}
+		} catch {
+			//
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const onConfirmBuy = () => {
+		modal.confirm({
+			title: "Xác nhận mua thông tin",
+			icon: <ExclamationCircleOutlined />,
+			content: "Bạn đồng ý sử dụng điểm để xem thông tin chi tiết khách hàng?",
+			okText: "Đồng ý",
+			cancelText: "Hủy",
+			onOk: onBuyInfo,
+		});
+	};
+
 	useEffect(() => {
 		const getData = async () => {
 			const categoryResponse = await httpAuthGet({
 				endpoint: apiFilterCategory,
 				params: {
 					parentId: searchParams.get("categoryId") || categoryIdDefault,
+					size: 12,
+					page: searchParams.get("page") || 1,
 				},
 			});
 			setCategories(categoryResponse?.data?.categories);
@@ -120,21 +162,37 @@ const Category = ({ title, list, count }) => {
 					{count > 0 ? (
 						<Row gutter={16} className="p-5">
 							{list?.map((item, i) => (
-								<Col xs={24} md={12} key={i}>
+								<Col xs={12} md={8} key={i}>
 									<div
 										className="p-3 border flex gap-5 cursor-pointer"
 										onClick={() => onSelectItem(item)}
 									>
 										<div>
-											<ShowAvatar src={item?.avatar} />
-										</div>
-										<div>
-											<h3 className="font-semibold text-base">{item?.name}</h3>
+											<h3 className="font-semibold text-base underline underline-offset-4">
+												{item?.name}
+											</h3>
 											<p>
-												<strong>Tỉnh/TP:</strong> {item?.province?.name}
+												<span className="mr-2">
+													<strong>Tuổi:</strong> {item?.age}
+												</span>
 											</p>
 											<p>
-												<strong>Quận/Huyện:</strong> {item?.district?.name}
+												<span>
+													<strong>Giới tính:</strong> {item?.gender}
+												</span>
+											</p>
+											<p>
+												<span className="mr-2">
+													<strong>Sdt:</strong> {item?.phone}
+												</span>
+											</p>
+											<p>
+												<span>
+													<strong>Email:</strong> {item?.email}
+												</span>
+											</p>
+											<p>
+												<strong>Tỉnh/TP:</strong> {item?.province?.name}
 											</p>
 										</div>
 									</div>
@@ -148,7 +206,7 @@ const Category = ({ title, list, count }) => {
 				<div className="mt-5">
 					{count > 0 && (
 						<Pagination
-							count={Math.ceil(count / 20)}
+							count={Math.ceil(count / 12)}
 							page={searchParams.get("page") || 1}
 							onChange={(e, page) => onChangePage(page)}
 							className="flex justify-center"
@@ -160,12 +218,36 @@ const Category = ({ title, list, count }) => {
 					title="Thông tin chi tiết"
 					onCancel={onCloseModal}
 					width={700}
+					footer={
+						<Button type="primary" onClick={onCloseModal}>
+							OK
+						</Button>
+					}
 				>
-					<div className="flex flex-col items-center">
-						<ShowAvatar src={itemSelected?.avatar} size={100} />
-						<p className="text-lg font-semibold mt-3">{itemSelected?.name}</p>
-						<Button type="text">Xem thêm...</Button>
-					</div>
+					<Spin spinning={loading}>
+						<div className="">
+							<ItemInfo label="Họ tên" value={itemSelected?.name} />
+							<ItemInfo label="Tuổi" value={itemSelected?.age} />
+							<ItemInfo label="Giới tính" value={itemSelected?.gender} />
+							<ItemInfo label="Số điện thoại" value={itemSelected?.phone} />
+							<ItemInfo label="Email" value={itemSelected?.email} />
+							<ItemInfo label="Địa chỉ" value={itemSelected?.address} />
+							<ItemInfo label="Tỉnh/TP" value={itemSelected?.province?.name} />
+							<ItemInfo
+								label="Quận/Huyện"
+								value={itemSelected?.district?.name}
+							/>
+							<ItemInfo label="Xã/Phường" value={itemSelected?.ward?.name} />
+							<ItemInfo label="Đường/Số nhà" value={itemSelected?.street} />
+							<ItemInfo label="MetaData" value={""} />
+						</div>
+						<div className="text-center mt-5">
+							<Button danger onClick={onConfirmBuy}>
+								Xem thông tin chi tiết
+							</Button>
+						</div>
+						{contextHolder}
+					</Spin>
 				</Modal>
 			</div>
 		</div>
